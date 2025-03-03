e1bb256107d7e6c1c74b23c7ea7b2301360b7159
Alex <worldgoggo05@gmail.com>
2025-03-03 23:45:59 +0900

    fix: installed packeages &modified format standards

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import {ConfigModule} from '@nestjs/config'
 
 @Module({
-  imports: [ConfigModule.forRoot()], // env ni uqish uchun
+  imports:[ConfigModule.forRoot()], // env ni uqish uchun
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NestarBatchService {
   getHello(): string {
-    return 'Hello from Batch Server!';
+    return 'Hello Batch 11!';
   }
 }
