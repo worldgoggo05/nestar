7b03ba65c7876eb69491fad70e729a194d81bcf8
Alex <worldgoggo05@gmail.com>
2025-03-11 05:39:35 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,38 +1,58 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.inputs';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
   constructor(private readonly memberService: MemberService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => Member)
   @UsePipes(ValidationPipe)
-  public async signup(@Args('input') input: MemberInput): Promise<string> {
-    console.log('Mutation: signup');
-    console.log("Signup:", input)
-    return this.memberService.signup();
+  public async signup(@Args('input') input: MemberInput): Promise<Member> {
+    try {
+      console.log('Mutation: signup');
+      console.log('Input:', input);
+      return this.memberService.signup(input);
+    } catch (err) {
+      console.log('Error, signup:', err);
+      throw new InternalServerErrorException(err);
+    }
   }
 
   @Mutation(() => String)
   @UsePipes(ValidationPipe)
   public async login(@Args('input') input: LoginInput): Promise<string> {
-    console.log('Mutation: login');
-    console.log("Login:", input)
-    return this.memberService.login();
+    try {
+      console.log('Mutation: login');
+      return this.memberService.login();
+    } catch (err) {
+      console.log('Error, login:', err);
+      throw new InternalServerErrorException(err);
+    }
   }
 
   @Mutation(() => String)
   public async updateMember(): Promise<string> {
-    console.log('Mutation: updateMember');
-    return this.memberService.updateMember();
+    try {
+        console.log('Mutation: updateMember');
+        return this.memberService.updateMember();
+      } catch (err) {
+        console.log('Error, updateMember:', err);
+        throw new InternalServerErrorException(err);
+      }
   }
 
   @Query(() => String)
   public async getMember(): Promise<string> {
-    console.log('Query: getMember');
-    return this.memberService.getMember();
+    try {
+        console.log('Query: getMember');
+        return this.memberService.getMember();
+      } catch (err) {
+        console.log('Error, getMember:', err);
+        throw new InternalServerErrorException(err);
+      }
   }
 }
 
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,16 +1,26 @@
 
-import {  Injectable } from '@nestjs/common';
+import {  BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.inputs';
 
 @Injectable()
 export class MemberService {
 
-  constructor(@InjectModel("Member") private readonly memberModel: Model<null>) {}
+  constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) {}
 
-  public async signup(): Promise<string> {
-    return 'signup executed!';
-  }
+  public async signup(input: MemberInput): Promise<Member> {
+    // Hash password
+    try {
+        const result = await this.memberModel.create(input);
+        // Authentication via TOKEN
+        return result
+    } catch (err) {
+        console.log('Error, Service.model:', err);
+        throw new BadRequestException(err);
+    }
+}
 
   public async login(): Promise<string> {
     return 'login executed!';
