5fef360bdbf4ad8663f562e289f17a07ef44c7fa
Alex <worldgoggo05@gmail.com>
2025-03-14 20:30:29 +0900

    feat:hash password via bycrpt

--- apps/nestar-api/src/components/auth/auth.module.ts
+++ apps/nestar-api/src/components/auth/auth.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  providers: [AuthService],
+  exports: [AuthService] // boshqa model ichida ishlatish uchun export qilindi -- member.model.ts
+})
 export class AuthModule {}
+
+
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -6,15 +6,19 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.inputs';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 
 @Injectable()
 export class MemberService {
-
-  constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) {}
+  constructor(
+    @InjectModel("Member") private readonly memberModel: Model<Member>,
+    private authService: AuthService,
+  ) {}
 
   public async signup(input: MemberInput): Promise<Member> {
     // Hash password
+    input.memberPassword = await this.authService.hashPassword(input.memberPassword)
     try {
         const result = await this.memberModel.create(input);
         // Authentication via TOKEN
@@ -40,7 +44,7 @@ public async login(input: LoginInput): Promise<Member> {
   }
 
   // TODO: Compare passwords
-  const isMatch = memberPassword === response.memberPassword;
+  const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword)
   if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
   return response;
