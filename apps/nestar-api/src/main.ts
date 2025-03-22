16503e11e89fd11ee5d819be0124d0b3290e014d
Alex <worldgoggo05@gmail.com>
2025-03-22 20:50:53 +0900

    feat: modify upload images configuration via GraphQL Upload Express

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress } from "graphql-upload"
+import * as express from "express"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // DTO validation globally integration
   app.useGlobalInterceptors(new LoggingInterceptor()) // Requestni log qilib beradi (Morgan kabi)
+  app.enableCors({origin: true, credentials: true})
+  app.use(graphqlUploadExpress({maxFileSize: 15000000, maxfiles: 10}))
+  app.use("/uploads", express.static("./uploads"))
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
