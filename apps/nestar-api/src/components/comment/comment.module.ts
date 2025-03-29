f41620b2d5d9abc86806c564a78f0f33a776006c
Alex <worldgoggo05@gmail.com>
2025-03-29 22:34:20 +0900

    feat: configure comment related logics

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { CommentResolver } from './comment.resolver';
 import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import CommentSchema from '../../schemas/Comment.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { PropertyModule } from '../property/property.module';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleModule } from '../board-article/board-article.module';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    {
+      name: "Comment",
+      schema: CommentSchema,
+    },
+  ]),
+  AuthModule,
+  MemberModule,
+  PropertyModule,
+  BoardArticleModule
+],
   providers: [CommentResolver, CommentService]
 })
 export class CommentModule {}
