68e263bd954370b1516ec010e44daa2d9dfac802
Alex <worldgoggo05@gmail.com>
2025-03-29 22:36:12 +0900

    feat:develop create,update and getComments APIs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -214,7 +214,7 @@ public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
   
   const {_id, targetKey, modifier} = input;
   return await this.memberModel
-  .findOneAndUpdate(_id, {$inc: {[targetKey]: modifier}}, {new: true})
+  .findByIdAndUpdate(_id, {$inc: {[targetKey]: modifier}}, {new: true})
   .exec();
 }
 }
